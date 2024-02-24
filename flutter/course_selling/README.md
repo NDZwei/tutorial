71d4394f657595db3eef43d9f2776715ba4f58a9
Nguyen Minh Duc <nmduc12k@gmail.com>
2024-02-24 17:16:47 +0700

    create project flutter: course selling

---
+++ flutter/course_selling/README.md
@@ -0,0 +1,5 @@
+# course_selling
+
+Course Selling Using Riverpod 2.0 dbestech
+src: https://youtu.be/_VShCq9b53Y?si=exZhccvPV0z18O_Y
+
---
+++ flutter/course_selling/android/app/src/main/java/com/ndz/course_selling/MainActivity.java
@@ -0,0 +1,6 @@
+package com.ndz.course_selling;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
